e23e0fa09e4beea2a1c95df566d06244c6c57ead
youziphpgit1 <964433503@qq.com>
2017-03-20 18:34:58 +0800

    Add files via upload

---
+++ Lib/Action/Home/IndexAction.class.php
@@ -0,0 +1,38 @@
+<?php
+class IndexAction extends Action{
+    public function index(){
+        $this->display();
+}
+    public function about(){
+        $this->display();
+    }
+    function news(){
+        $this->display();
+    }
+    function news_xx(){
+        $this->display();
+    }
+    function product(){
+        $this->display();
+    }
+    function product_xx(){
+        $this->display();
+    }
+    function khjz(){
+        $this->display();
+    }
+    function hzhb(){
+        $this->display();
+    }
+    function contach(){
+        $this->display();
+    }
+}
+
+
+
+
+
+
+
+
