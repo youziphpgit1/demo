ead2423a37535ac30e18ed06a54c3e0a40b9a1d5
youziphpgit1 <964433503@qq.com>
2017-03-20 18:26:06 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: youzi
+ * Date: 2017/3/20
+ * Time: 15:25
+ */
+header("content-type:text/html;charset=utf-8");
+define("APP_DEBUG",TRUE);
+require_once("./Thinkphp/ThinkPHP.php");
+
+
+
